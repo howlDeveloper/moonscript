62c9b192a1b2f82ce198948bbfc2beec13a7dac4
João Luis <125226749+howlDeveloper@users.noreply.github.com>
2023-05-07 19:05:50 -0300

    Update dist.lua

--- dist.lua
+++ dist.lua
@@ -5,3 +5,11 @@ moonscript.console:warn("hello world")
 moonscript.console:error("hello world")
 
 moonscript.console:count(1, 2)
+
+local tabela = {
+  [1] = "1",
+  [2] = "2",
+  [3] = "3",
+}
+
+moonscript.console:info(tabela)
