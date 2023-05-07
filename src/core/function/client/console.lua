8c9cf75b603a214b42a00f7140c7cbcace3a5cb4
João Luis <125226749+howlDeveloper@users.noreply.github.com>
2023-05-07 19:04:28 -0300

    Update console.lua

--- src/core/function/client/console.lua
+++ src/core/function/client/console.lua
@@ -24,17 +24,13 @@ function moonscript.console:warn(warnMessage)
     return opds("@moonscript Console:Warn ["..tostring(warnMessage).."]", 4, 255, 165, 0)
 end
 
-function moonscript.console:count(n1, n2, sym)
+function moonscript.console:count(n1, n2)
     if (not n1) then
         return opds("@moonscript bad Argument at 'console:count' [argument 1] got nil")
     end
 
     if (not n2) then
         return opds("@moonscript bad Argument at 'console:count' [argument 2] got nil")
-    end
-
-    if (not sym) then
-        return opds("@moonscript bad Argument at 'console:count' [argument 3] got nil")
     end
 
     if (type(n1) ~= "number" and type(n2) ~= "number") then
